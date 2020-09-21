85b67c2b2f4cf96b2de507770c84ec697cd41725
jrdnc <jrdnblvn@gmail.com>
2020-09-21 15:50:23 -0500

    set up the timer

--- script.js
+++ script.js
@@ -4,12 +4,25 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
-
+var timer = [0, 0, 0, 0];
 // Add leading zero to numbers 9 or below (purely for aesthetics):
-
+function leadingZero(time) {
+    if (time <= 9) {
+        time = "0" + time;
+    }
+    return time;
+}
 
 // Run a standard minute/second/hundredths timer:
+function runTimer() {
+    let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
+    theTimer.innerHTML = currentTime;
+    timer[3]++; // counts every 1/1000 sec
 
+    timer[0] = Math.floor((timer[3] / 100) / 60); // total millisec / 100 == total sec, then total sec / 60 to get minutes
+    timer[1] = Math.floor((timer[3] / 100) - (timer[0] * 60)); // total millisec / 100 == total sec, then every 60 sec, reset value to 0
+    timer[2] = Math.floor((timer[3] - (timer[1] * 100) - timer[0] * 6000)); // reset value every time sec and/or minutes reach 60
+}
 
 // Match the text entered with the provided text on the page:
 function spellCheck() {
@@ -21,6 +34,9 @@ function spellCheck() {
 function start() {
     // make sure only detecting very first keypress
     let textEnteredLength = testArea.value.length;
+    if (textEnteredLength === 0) {
+        setInterval(runTimer, 10); // run funciton 'runTimer' every 1/1000 sec
+    }
     console.log(textEnteredLength);
 }
 
